df6bf54062cb49559c30690c009fd87d67ababc8
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-08-21 22:01:51 +0500

    .

---
+++ 344. Reverse String/app.py
@@ -0,0 +1,12 @@
+class reverse:
+    def reverse_string(self, s):
+        l, r = 0 , len(s)-1
+        while l < r :
+            s[l], s[r] = s[r], s[l]
+            l, r = l+1, r-1
+        return s
+
+reverse = reverse()
+s = ['h','e','l','l','o']
+result = reverse.reverse_string(s)
+print(result)
