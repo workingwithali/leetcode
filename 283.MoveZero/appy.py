7ffe4962cb69b0a1f410800d49fe8d3b7bfec9a8
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-08-24 16:21:52 +0500

    update

--- 283.MoveZero/appy.py
+++ 283.MoveZero/appy.py
@@ -5,5 +5,9 @@ class solution:
             if nums[r] != 0:
                 nums[l], nums[r] = nums[r], nums[l]
                 l +=1
+        return nums
 
 solution =solution() 
+arr = [1,2,3,0,0,4,5,6]
+result = solution.zero(arr)
+print(result)
