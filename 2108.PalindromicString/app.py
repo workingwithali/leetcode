10feba9bcd5fa7821f5179dc2bcbb407463b6d70
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-08-26 20:24:03 +0500

    update

--- 2108.PalindromicString/app.py
+++ 2108.PalindromicString/app.py
@@ -1,4 +1,16 @@
 class solution:
     def string(self,words):
         for w in words:
-            
+            l, r = 0 ,len(w)-1
+            while w[l] == w[r]:
+                if l >= r:
+                    return w
+                l +=1
+                r -=1
+        return ""
+    
+solution = solution()
+arr = ['adfa', 'ads','aba','ere' ]
+result = solution.string(arr)
+print(result)
+
