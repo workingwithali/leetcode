c90c1f891dc4248ec342c2c7e04377f2ae15407a
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-08-29 21:50:02 +0500

    update

--- 844. Backspace String Compare/app.py
+++ 844. Backspace String Compare/app.py
@@ -11,7 +11,4 @@ class Solution:
         print(build(t))
         
         return build(s) == build(t)
-solution = Solution()
-print(solution.backspaceCompare("ab#c", "ad#c"))  # Output: True
-print(solution.backspaceCompare("ab##", "c#d#"))  # Output: True
-print(solution.backspaceCompare("a#c", "b"))      # Output: False
+
