b1a2f2d2d8719e27a199d3022da4568d254b7688
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-08-30 20:19:24 +0500

    update

--- 1662.TwoStringArraysEquivalent/appy.py
+++ 1662.TwoStringArraysEquivalent/appy.py
@@ -1,5 +1,13 @@
 class solution:
     def string(self, word1,word2):
-        w1 = w2 = 0
-        i = j = 0
-        while 
+        str1 = str2 = ""
+        for word in word1:
+            str1 += word
+        for word in word2:
+            str2 += word
+        return str1 == str2
+solution = solution()
+word1 = ['ab','c']
+word2 = ['a','bc']
+result = solution.string(word1 , word2)
+print (result)
