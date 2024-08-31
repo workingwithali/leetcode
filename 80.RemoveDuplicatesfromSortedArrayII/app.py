d1f6279b1305a55be45d3b774db59d536f6a67ea
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-08-31 16:54:24 +0500

    update

--- 80.RemoveDuplicatesfromSortedArrayII/app.py
+++ 80.RemoveDuplicatesfromSortedArrayII/app.py
@@ -1,21 +1,17 @@
 class Solution:
     def removeDuplicates(self, nums):
         if len(nums) <= 2:
-            return len(nums)
-        
-        index = 2
-        
+            return len(nums)        
+        index = 2        
         for i in range(2, len(nums)):
             
             if nums[i] != nums[index - 2]:
                 nums[index] = nums[i]
-                index += 1
-                
+                index += 1                
         return index
 solution = Solution()
 nums = [1, 1, 1, 2, 2, 3]
 length = solution.removeDuplicates(nums)
 
-# Output the results
 print("Length of array after removing duplicates:", length)
 print("Modified array:", nums[:length])
