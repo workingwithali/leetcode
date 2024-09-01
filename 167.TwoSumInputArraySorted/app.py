f0050a46a8e91d781f9126a931a99b5dc7e6da50
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-09-01 21:26:51 +0500

    update

--- 167.TwoSumInputArraySorted/app.py
+++ 167.TwoSumInputArraySorted/app.py
@@ -1,16 +1,17 @@
 class solution :
     def TwoSum(self, numbers, target):
-        l , r = 0 , len(numbers)-1
+        l, r = 0, len(numbers) - 1
         while l < r:
             sum = numbers[l] + numbers[r]
             if sum > target:
                 r -=1
             elif sum < target:
-                r +=1
+                l +=1
             else:
-                return [l+1, r+1]
+                return [l + 1, r + 1]
 
 solution = solution()
 numbers = [1,2,3,4,5,6,7]
 target = 9
-
+result = solution.TwoSum(numbers, target)
+print(result)
