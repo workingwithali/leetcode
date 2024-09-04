09b8aba821c0cf9e8dc0f5e02d8d1fc94618be7e
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-09-04 16:39:55 +0500

    update

--- 11.ContainerWithMostWater/app.py
+++ 11.ContainerWithMostWater/app.py
@@ -1,4 +1,16 @@
 class solution :
     def water(self, height):
         l, r = 0, len(height) - 1
-        
+        res = 0
+        while l < r:
+            area = r-l * min(height[l], height[r])
+            res = max [area,res]
+            if height[l] < height[r]:
+                l +=1
+            else: 
+                r -=1
+        return res
+    
+height = [2,3,5,7,9,3,5,10,]
+
+
