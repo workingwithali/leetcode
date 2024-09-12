232eef2827fcc6f0cc84a59e4a86fcb0f9dc5028
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-09-12 11:58:09 +0500

    update

--- 424.LongestRepeatingCharacterReplacement/app.py
+++ 424.LongestRepeatingCharacterReplacement/app.py
@@ -1,28 +1,24 @@
-class solution:
+class Solution:
     def longest(self, s, k):
         count = {}
         res = 0
         l = 0
         max_char = 0
         for r in range(len(s)):
-            count [s[r]] =1 + count[s[r],0]
-            max_char = max(max_char, count[s[r]]) 
+            count [s[r]] =1 + count.get(s[r],0)
+            if (r-l+1) - max(count.values())>k:
+                count[s[l]] -=1
+                l+=1
 
 
 
             res = max(res,r-l+1) 
+        return res
 
 
 
-# Create an instance of the Solution class
 sol = Solution()
-
-# Define the input string and k value
 s = "AABABBA"
 k = 1
-
-# Call the characterReplacement function
-result = sol.characterReplacement(s, k)
-
-# Print the result
-print(result)  # Output: 4
+result = sol.longest(s,k)
+print(result) 
