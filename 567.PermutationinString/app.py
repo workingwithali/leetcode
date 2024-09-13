f14eade4dd13c6afe7e9d9a86c386995a88b4c63
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-09-13 16:49:37 +0500

    update

--- 567.PermutationinString/app.py
+++ 567.PermutationinString/app.py
@@ -11,19 +11,18 @@ def checkInclusion(s1: str, s2: str) -> bool:
     if s1_count == s2_count:
         return True
     
-    print(s1_count)
-    print(s2_count)
+    # print(s_count)
     
     for i in range(len1, len2):
         # Add the next character in the window
         s2_count[s2[i]] += 1
-        print(s2_count)
         # Remove the character that is no longer in the window
         s2_count[s2[i - len1]] -= 1
         
         # If the count becomes 0, remove the key from the dictionary
         if s2_count[s2[i - len1]] == 0:
             del s2_count[s2[i - len1]]
+        print(s2_count)
         
         # If the current window matches s1_count, return True
         if s1_count == s2_count:
