3bdf48c8dd3c97ae6461124555aec9be6275295c
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-09-19 07:52:44 +0500

    update

--- 1456.MaximumNumberofVowels/app.py
+++ 1456.MaximumNumberofVowels/app.py
@@ -1,2 +1,24 @@
-class solutin:
-    def vowel()
+class solution:
+    def vowel(self,s,k):
+        vowels = {'a', 'e', 'i', 'o', 'u'}
+        l , cnt , res = 0, 0, 0
+        for r in range(len(s)):
+            cnt += 1 if s[r] in vowels else 0
+            print(cnt)
+            if r - l + 1 > k:
+                cnt -=1 if s[l] in vowels else 0
+                print(cnt)
+                l +=1
+            res = max(res, cnt)
+        return res
+
+
+
+
+
+
+solution = solution()
+s = "abciiidef"
+k = 3
+result = solution.vowel(s,k)
+print(result)
