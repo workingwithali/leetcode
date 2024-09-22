a3cd9a23f4d55b51ff5deb863e38aa92dedcca7a
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-09-22 21:30:34 +0500

    update

--- 209.MinimumSizeSubarraySum/app.py
+++ 209.MinimumSizeSubarraySum/app.py
@@ -1,4 +1,16 @@
 class solution:
     def size(target,nums):
-        
+        l , total = 0,0
+        res = float('inf')
+        for r in range(len(nums)):
+            total += nums[r]
+            while total >= target:
+                res = min(res , r-l+1)
+                total -= nums[l]
+                l+=1
+        return 0 if res == float('int') else res
+    
+solution = solution()
+
+
         
