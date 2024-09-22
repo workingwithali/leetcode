2d15c5ec6d0c313b2ae9e0b58efc22f42f464243
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-09-22 18:04:56 +0500

    update

--- 1888.MinimumNumberofFlipstoMaketheBinaryStringAlternating/app.py
+++ 1888.MinimumNumberofFlipstoMaketheBinaryStringAlternating/app.py
@@ -7,4 +7,12 @@ class solution:
         for i in range(len(s)):
             alt1 += '1' if i%2 else '0'
             alt2 += '0' if i%2 else '1'
+        res = len(s)
+        d1 ,d2 = 0 ,0
+        l = 0
+        for r in range(len(s)):
+            if s[r] != alt1[r]:
+                d1 +=1
+            if s[r] != alt2[r]:
+                d2 +=1
             
