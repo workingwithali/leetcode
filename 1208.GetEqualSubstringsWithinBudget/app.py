51feb747ba7fdea5f0c3712df723f7087607f2ec
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-09-28 11:25:29 +0500

    update

--- 1208.GetEqualSubstringsWithinBudget/app.py
+++ 1208.GetEqualSubstringsWithinBudget/app.py
@@ -4,9 +4,15 @@ class solution:
         l = 0
         res = 0
         for r in range(len(s)):
-            curCost += abs(ord(s[r])-ord(t[s]))
+            curCost += abs(ord(s[r])-ord(t[r]))
             while curCost > maxCost:
                 curCost -= abs(ord(s[l])-ord(t[l]))
+                l+=1
             res = max(res, r-l+1)
         return res
-    
+s = "abcd"
+t = "bcdf"
+maxCost = 3
+solution = solution()
+result = solution.equat(s,t,maxCost)
+print(result)
