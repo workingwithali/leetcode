c87c26337d481442597a7fa31eb9c551335e9771
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-09-29 12:00:51 +0500

    update

---
+++ 930.BinarySubarraysWithSum/app.py
@@ -0,0 +1,21 @@
+class solution:
+    def sub(self,nums,goal):
+        x = goal
+        if x < 0 return 0
+        l = 0
+        sum = 0
+        for r in range(len(nums))
+
+
+
+
+
+
+
+
+
+nums = [1,0,1,0,1]
+goal = 2
+solution =solution()
+result = solution.sub(nums,goal)
+print(result)
