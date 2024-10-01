368313b45af56c94e555d4866a6ffb0b6da10fa5
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-10-01 19:00:48 +0500

    update

--- 1160.FindWordsThatCanBeFormedbyCharacters/appy.py
+++ 1160.FindWordsThatCanBeFormedbyCharacters/appy.py
@@ -1,2 +1,16 @@
 from collections import Counter
-c
+class solution:
+    def word(self,words,chars):
+        count = Counter(chars)
+        res = 0
+        for w in words:
+            cur_word = Counter()
+            good = True
+            for c in w:
+                cur_word[c] +=1
+                if c not in count or cur_word[c] > count[c]:
+                    good = False
+                    break
+            if good:
+                res += len(w)
+        return res
