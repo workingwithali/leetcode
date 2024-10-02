28d182f7a361a5b21ac36d8031f489263981dcb5
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-10-02 21:02:06 +0500

    update

--- 2264.Largest3Same-DigitNumberinString/appy.py
+++ 2264.Largest3Same-DigitNumberinString/appy.py
@@ -1,7 +1,12 @@
 class solution:
     def three(self,num):
         res = '0'
-        for i in range(len(num)):
+        for i in range(len(num)-2):
             if num[i] == num[i+1] == num[i+2]:
                 res = max(res, num[i:i+3])
         return '' if res == '0' else res
+    
+num = "6777133339"
+solution = solution()
+result = solution.three(num)
+print(result)
