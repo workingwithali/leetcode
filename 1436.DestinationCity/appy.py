ecd35d052c7b7783d5ad2224e5fc61155ce23f6c
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-10-03 20:43:31 +0500

    update

--- 1436.DestinationCity/appy.py
+++ 1436.DestinationCity/appy.py
@@ -6,4 +6,6 @@ class solution:
         for p in paths:
             if p[1] not in s:
                 return p[1] 
- paths = [["London","New York"],["New York","Lima"],["Lima","Sao Paulo"]]
+paths = [["London","New York"],["New York","Lima"],["Lima","Sao Paulo"]]
+solution = solution()
+reult =
