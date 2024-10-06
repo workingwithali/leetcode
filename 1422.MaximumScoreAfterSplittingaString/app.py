df679a6e394940764a9425080cb218f22abfa355
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-10-07 00:04:32 +0500

    update

--- 1422.MaximumScoreAfterSplittingaString/app.py
+++ 1422.MaximumScoreAfterSplittingaString/app.py
@@ -4,4 +4,9 @@ class solution :
         one = s.count('1')
         res = 0
         for n in range(len(s)-1):
-            
+            if s[n] == "0":
+                zero += 1
+            else:
+                one -=1
+            res = max(res , zero + one)
+        return res
