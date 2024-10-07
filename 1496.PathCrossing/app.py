b0ea09c40ebe4fc4b07f47cd605f3b9abba3cf15
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-10-07 15:21:03 +0500

    update

--- 1496.PathCrossing/app.py
+++ 1496.PathCrossing/app.py
@@ -1,8 +1,24 @@
 class solultion:
     def path(self,path):
+        dir = {
+            "N":[0,1],
+            "S":[0,-1],
+            "E":[1,0],
+            "W":[-1,0],
+        }
+        vist = set()
+        x , y = 0 , 0
+        for n in path:
+            vist.add((x,y))
+            print(vist)
+            dx, dy = dir[n]
+            x , y = x + dx, y + dy
+            if (x,y) in vist:
+                return True
+        
         return False
 
 solultion = solultion()
-path = "NES"
+path = "NESW"
 result = solultion.path(path)
 print(result)
