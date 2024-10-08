a110654c190673c7deb9a365bf25ff29eeeee06d
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-10-08 21:17:32 +0500

    update

--- 1758.MinimumChangesToMakeAlternatingBinaryString/app.py
+++ 1758.MinimumChangesToMakeAlternatingBinaryString/app.py
@@ -5,7 +5,7 @@ class solultion:
             if n % 2:
                 count += 1 if s[n] =='0' else 0
             else:
-                count += 1 if s[n] == '1' else 
+                count += 1 if s[n] == '1' else 0
         return min(count, len(s)-count)
 s = "0100"
 solultion = solultion()
