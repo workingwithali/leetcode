df4ebe06748372cf02520c145a878ac4b43f115f
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-10-10 21:36:03 +0500

    update

--- 1624.LargestSubstringBetweenTwoEqualCharacters/app.py
+++ 1624.LargestSubstringBetweenTwoEqualCharacters/app.py
@@ -1,4 +1,10 @@
 class solution:
     def substing(self,s):
         char = {}
-        
+        res = -1
+        for i,c in enumerate(s):
+            if c in char:
+                res = max(res,i-char[c]-1)
+            else:
+                char[c] = i
+        return res        
