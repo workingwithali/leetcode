49dda28f8cb0b42e1862143aafb9da0339834691
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-10-15 20:36:01 +0500

    update

---
+++ 387.FirstUniqueCharacterString/app.py
@@ -0,0 +1,13 @@
+
+from collections import defaultdict
+
+def unique(s):
+   
+    count = defaultdict(int)
+    for char in s:
+        count[char] += 1
+    for i,char in enumerate(s):        
+        if count[char] == 1:
+            return i
+    return -1
+
