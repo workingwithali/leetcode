55888b265ab5e28c3ca87424a0333c420b949839
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-10-16 22:46:46 +0500

    update

--- 349.IntersectionofTwoArrays/app.py
+++ 349.IntersectionofTwoArrays/app.py
@@ -1,4 +1,15 @@
 class solution:
     def intersection(self,nums1,nums2):
         seen = set(nums1)
-        
+        res = []
+        for n in nums2:
+            if n in seen:
+                res.append(n)
+                seen.remove(n)
+        return res
+    
+solution = solution()
+nums1 = [1,2,2,1]
+nums2 = [2,2]
+result = solution.intersection(nums1,nums2)
+print(result)
