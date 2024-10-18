b5af2eaa48e6e2087c03e96a24c8b4e2cbc9207e
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-10-18 20:49:56 +0500

    update

---
+++ 1822.SignProductArray/app.py
@@ -0,0 +1,8 @@
+class solution:
+    def sign(self,nums):
+        sign = 1
+        for i in nums:
+            if i == 0:
+                return 0
+            elif i<0:
+                sign = -sign
