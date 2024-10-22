f039d9dba634f45dab4175a797830286e663069d
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-10-22 17:38:13 +0500

    update

--- 150.EvaluateReversePolishNotation/app.py
+++ 150.EvaluateReversePolishNotation/app.py
@@ -1,3 +1,14 @@
 def resvers(tokens):
-    for c in token:
-        if c ==
+    stack = []
+    for c in tokens:
+        if c == "+":
+            stack.append(stack.pop()+stack.pop())
+        elif c == "-":
+            a, b = stack.pop(), stack.pop()
+            stack.append(b-a)
+        elif c == "*":
+            stack.append(stack.pop()*stack.pop())
+        elif c == "/":
+            a, b = stack.pop(), stack.pop()
+            stack.append(int(float(b))/a)
+        
