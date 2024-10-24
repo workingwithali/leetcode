3a16cdf68edcba6819a4d1f86f2a0100c50fad4c
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-10-24 20:57:56 +0500

    update

--- 2390.RemovingStarsFromString/app.py
+++ 2390.RemovingStarsFromString/app.py
@@ -1,3 +1,9 @@
 class solutin:
     def remove(self,s):
-        
+        stack = []
+        for c in s:
+            if c == '*':
+                stack.pop()
+            else:
+                stack.append(c)
+        return ''.join(stack)
