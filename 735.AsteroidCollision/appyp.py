7235941a74bb803551b002797d2f604cf6b64660
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-10-26 08:59:14 +0500

    update

--- 735.AsteroidCollision/appyp.py
+++ 735.AsteroidCollision/appyp.py
@@ -1,1 +1,6 @@
-class so
+class solution:
+    def collision(self, asteroid):
+        stack = []
+        for s in asteroid:
+            while stack and s < 0 and stack[-1] > 0:
+                di
