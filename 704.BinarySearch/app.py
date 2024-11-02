e8a178d4e0c82b52410ed749e8f30bb4861b8eec
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-11-02 17:04:29 +0500

    update

--- 704.BinarySearch/app.py
+++ 704.BinarySearch/app.py
@@ -1,3 +1,12 @@
 class solution :
     def binary(self, nums,target):
-        
+        left, right = 0 ,len(nums)
+        while left<=right:
+            mid= (right+left)//2
+            if nums[mid]==target:
+                return mid
+            elif nums[mid]<target:
+                left = mid +1
+            else:
+                right = mid -1
+        return -1
