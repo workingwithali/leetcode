ff746b6813a635701c159167504ea454121d58aa
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-11-03 09:34:38 +0500

    update

--- 35.SearchInsertPosition/app.py
+++ 35.SearchInsertPosition/app.py
@@ -4,4 +4,9 @@ class solution:
         while left<=right:
             mid = (right+left)//2
             if nums[mid] == target:
-                
+                return mid
+            elif nums[mid]<target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return left
