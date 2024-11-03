52c3d18b0dc3bea99fd29810a22e6a08e6be10eb
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-11-03 09:35:57 +0500

    update

--- 35.SearchInsertPosition/app.py
+++ 35.SearchInsertPosition/app.py
@@ -10,3 +10,8 @@ class solution:
             else:
                 right = mid - 1
         return left
+    
+solution = solution()
+nums = [1,3,5,6]
+target = 5
+result = solution.insert(nums,target)
