c835588464da83f716116594335782e17ea60847
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-11-05 22:59:16 +0500

    update

--- 977.SquaresSortedArray/app.py
+++ 977.SquaresSortedArray/app.py
@@ -3,5 +3,15 @@ class solution:
         n = len(nums)
         result = [0]*n
         l, r = 0, n-1
-        
+        position = n-1
+        while l<=r:
+            left_saq = nums[l]**2
+            right_saq = nums[r]**2
+            if left_saq > right_saq:
+                result[position] = left_saq
+                l+=1
+            else:
+                result[position] = right_saq
+                r-=1
+            position -=1
         return result
