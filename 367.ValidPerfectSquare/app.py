3b0f91ce4771a8a244c55d265786dde519e29af8
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-11-06 22:28:36 +0500

    update

---
+++ 367.ValidPerfectSquare/app.py
@@ -0,0 +1,13 @@
+class solution:
+    def saq(self, num):
+        l, r = 0, num
+        while l<=r:
+            mid = (l+r)//2
+            saq = mid * mid
+            if saq == mid:
+                return True
+            elif saq < mid:
+                l = mid + 1
+            else:
+                r = mid - 1
+        return False
