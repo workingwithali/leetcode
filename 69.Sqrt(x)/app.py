7ac787b6f42ab6b917d393c532ab6dcf1df0cd06
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-11-07 15:53:47 +0500

    update

---
+++ 69.Sqrt(x)/app.py
@@ -0,0 +1,12 @@
+class solution:
+    def sq(self,x):
+        l, r = 0 , x
+        while l<=r:
+            mid = (r+l)//2
+            saq = mid*mid
+            if saq == x:
+                return mid
+            elif saq<mid:
+                l = mid +1
+            else:
+                r = mid - 1
