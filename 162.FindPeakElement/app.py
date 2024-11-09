8cc2e3e7cdc0bb5dcd686f4acd8f2032637691ef
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-11-09 15:52:37 +0500

    update

--- 162.FindPeakElement/app.py
+++ 162.FindPeakElement/app.py
@@ -1,3 +1,7 @@
 class solution:
     def find(self,nums):
-        l,r
+        l,r=0,len(nums)
+        while l<r:
+            mid = (r+l)//2
+            if nums[mid]<nums[mid+1]:
+                l = mid+1
