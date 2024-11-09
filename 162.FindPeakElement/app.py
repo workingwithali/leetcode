f5ba38fa2d40ff39044b2b8d606af0bdee0a9604
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-11-09 16:02:37 +0500

    update

--- 162.FindPeakElement/app.py
+++ 162.FindPeakElement/app.py
@@ -1,14 +1,15 @@
-class solution:
-    def find(self,nums):
-        l,r=0,len(nums)
-        while l<r:
-            mid = (r+l)//2
-            if nums[mid]<nums[mid+1]:
-                l = mid+1
+class Solution:
+    def find(self, nums):
+        l, r = 0, len(nums) - 1  # Set right boundary to len(nums) - 1
+        while l < r:
+            mid = (l + r) // 2
+            if nums[mid] < nums[mid + 1]:  # Compare mid with mid + 1
+                l = mid + 1
             else:
-                r=mid
-        return l
-solution =solution()
+                r = mid
+        return l  # l and r converge to the peak element's index
+
+solution = solution()
 nums = {1,2,3,5,3,4,3,4}
 r = solution.find(nums)
 print(r)
