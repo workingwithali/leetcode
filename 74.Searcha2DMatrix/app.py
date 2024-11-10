9dc9797d46745ca30cf85064b77b6429451a694a
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-11-10 07:57:00 +0500

    update

---
+++ 74.Searcha2DMatrix/app.py
@@ -0,0 +1,7 @@
+class solution:
+    def d(self,matrix,target):
+        for r in range(len(matrix)):
+            for c in range(len(matrix[0])):
+                if matrix[r][c] == target:
+                    return True
+        return False
