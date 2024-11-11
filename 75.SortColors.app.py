ed1d7f2cef7f4ab8b8c8948c3212ed78300ce9c7
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-11-11 23:25:23 +0500

    update

--- 75.SortColors.app.py
+++ 75.SortColors.app.py
@@ -2,4 +2,8 @@ class solution:
     def sort(self,nums):
         l,m,h=0,0,len(nums)-1
         while m<=h:
-            if 
+            if nums[m]==0:
+                nums[l],nums[m]=nums[m],nums[l]
+                l+=1
+                m+=1
+            elif nums[]
