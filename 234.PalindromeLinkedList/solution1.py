85be50311188292ec6b74ce0da562f9faac5d468
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-11-16 14:34:55 +0500

    update

--- 234.PalindromeLinkedList/solution1.py
+++ 234.PalindromeLinkedList/solution1.py
@@ -1,3 +1,25 @@
 class solution:
     def linked(self,head):
+        # slow is mide of linked list 
+        slow , fast = head,head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
         
+        # reverse linklist
+        prev = None
+        while slow:
+            nextp = slow.next
+            slow.next = prev
+            prev = slow
+            slow = nextp
+
+        # check palindrome
+        l ,r = head , prev
+        while r:
+            if l.val != r.val:
+                return False
+            l = l.next
+            r = r.next
+        return True
+
