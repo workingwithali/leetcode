7abe00e01b5d4d04f29d0ab541bc118caf59084b
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-11-16 14:49:04 +0500

    update

--- 234.PalindromeLinkedList/solution2.py
+++ 234.PalindromeLinkedList/solution2.py
@@ -2,3 +2,27 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
+
+class solution:
+    def isPalindrome(self , head):
+        
+        
+
+def create_linked_list(elements):
+    if not elements:
+        return None
+    head = ListNode(elements[0])
+    current = head
+    for value in elements[1:]:
+        current.next = ListNode(value)
+        current = current.next
+    return head
+
+
+# Test the code
+elements = [1, 2, 2, 1]
+head = create_linked_list(elements)
+solution = Solution()
+result = solution.isPalindrome(head)
+print(result)  # Expected output: True
