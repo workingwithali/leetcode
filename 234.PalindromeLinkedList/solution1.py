43c50fae82459dd66fd621d95fc8d1a96f06a26c
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-11-16 14:46:42 +0500

    update

--- 234.PalindromeLinkedList/solution1.py
+++ 234.PalindromeLinkedList/solution1.py
@@ -1,12 +1,17 @@
-class solution:
-    def linked(self,head):
-        # slow is mide of linked list 
-        slow , fast = head,head
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def isPalindrome(self, head):
+        # Find the middle of the linked list
+        slow, fast = head, head
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-        
-        # reverse linklist
+
+        # Reverse the second half of the linked list
         prev = None
         while slow:
             nextp = slow.next
@@ -14,17 +19,29 @@ class solution:
             prev = slow
             slow = nextp
 
-        # check palindrome
-        l ,r = head , prev
-        while r:
-            if l.val != r.val:
+        # Check palindrome
+        left, right = head, prev
+        while right:  # Only need to compare till the end of the reversed half
+            if left.val != right.val:
                 return False
-            l = l.next
-            r = r.next
+            left = left.next
+            right = right.next
         return True
-    
 
-solution = solution()
-head = [1,2,2,1]
-r = solution.linked(head)
+# Helper function to create a linked list from a list
+def create_linked_list(elements):
+    if not elements:
+        return None
+    head = ListNode(elements[0])
+    current = head
+    for value in elements[1:]:
+        current.next = ListNode(value)
+        current = current.next
+    return head
 
+# Test the code
+elements = [1, 2, 2, 1]
+head = create_linked_list(elements)
+solution = Solution()
+result = solution.isPalindrome(head)
+print(result)  # Expected output: True
