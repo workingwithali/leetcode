9b4ae5ea2b1502b2db049b0d2f0918b56b3870fc
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-11-20 16:48:24 +0500

    update

--- 160.IntersectionLinkedLists/app.py
+++ 160.IntersectionLinkedLists/app.py
@@ -2,4 +2,6 @@ class solution:
     def inter(slef , heada, headb):
         l1 ,l2 = heada , headb
         while l1 != l2:
-            l1 = l1.
+            l1 = l1.next if l1 else heada
+            l2 = l2.next if l2 else headb
+        return l1
