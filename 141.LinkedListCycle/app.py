e9241e068a52ec7cb0493764afff83a7b08114e4
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-11-24 21:26:55 +0500

    update

--- 141.LinkedListCycle/app.py
+++ 141.LinkedListCycle/app.py
@@ -3,4 +3,8 @@ class solution:
         seen = set()
         cur = head
         while cur:
-            
+            if cur in seen:
+                return True
+            seen.add(cur)
+            cur = cur.next
+        return False
