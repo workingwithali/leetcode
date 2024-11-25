439149fec8bbae9c69b698e800ad3b9ab37883b3
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-11-25 12:32:26 +0500

    update

---
+++ 2130.MaximumTwinSumLinkedList/app.py
@@ -0,0 +1,14 @@
+class solution:
+    def twin(self, head):
+        slow , fast =head, head
+        prev = None
+        while fast and fast.next:
+            fast = fast.next.next
+            nextp = slow.next
+            slow.next = prev
+            prev = slow 
+            slow = nextp
+
+        res= 0
+        while slow:
+            
