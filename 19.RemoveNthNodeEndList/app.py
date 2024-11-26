3b58f53b33ebbd4a64ad5d51ff50e57d72583097
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-11-26 15:58:22 +0500

    update

---
+++ 19.RemoveNthNodeEndList/app.py
@@ -0,0 +1,16 @@
+class solution:
+    def remove(self, head,n):
+        N =0 
+        cur = head
+        while cur:
+            N+=1
+            cur = cur.next
+        remove = N-n
+        if remove ==0:
+            return head.next
+        for i in range(N-1):
+            if (i+1)==remove:
+                cur.next = cur.next.next
+                break
+            cur = cur.next
+        return head
