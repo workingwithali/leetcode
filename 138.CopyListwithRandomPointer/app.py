a3b7aced9cf4481320fb246adbabc66f9bfff5b6
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-11-28 17:05:53 +0500

    update

--- 138.CopyListwithRandomPointer/app.py
+++ 138.CopyListwithRandomPointer/app.py
@@ -1,7 +1,11 @@
 class solution:
-    def copy(self,head):
-        if not head:
-            return None
-        cur = head
-        while head
-        new_node = Node()
+    def __init__(self, val: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = val
+        self.next = next
+        self.random = random
+def copy(self,head):
+    if not head:
+        return None
+    cur = head
+    while head
+    new_node = Node()
