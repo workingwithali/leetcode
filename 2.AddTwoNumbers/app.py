8444c5e04767486c32dc276f1e0c33a4f6acd9c1
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-11-29 17:08:38 +0500

    update

--- 2.AddTwoNumbers/app.py
+++ 2.AddTwoNumbers/app.py
@@ -8,4 +8,18 @@ def add(l1,l2):
     cur = dummy
     carry = 0
     while l1 or l2 or carry:
-        
+        sum = 0+ carry
+        if l1:
+            sum += l1.val
+            l1 = l1.next
+
+
+        if l2:
+            sum += l2.val
+            l2 = l2.next
+
+
+        carry = sum//10
+        cur = ListNode(sum%10)
+        cur = cur.next
+    return dummy.next
