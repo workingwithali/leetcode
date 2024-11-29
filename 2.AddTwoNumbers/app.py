639c2ba23e42168fa8b306aab70d5cfc025428a9
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-11-29 17:02:25 +0500

    update

--- 2.AddTwoNumbers/app.py
+++ 2.AddTwoNumbers/app.py
@@ -1,2 +1,8 @@
-class solution:
-    
+class ListNode:
+    def __init__(self,val=0,next=None) -> None:
+        self.val = val
+        self.next = next
+
+def add(l1,l2):
+    dummy = ListNode(0)
+    cur = dummy
