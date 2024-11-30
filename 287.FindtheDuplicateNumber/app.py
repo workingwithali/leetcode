0c41076cd02372315b1db25a33bc2ef68f869dff
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-11-30 17:18:18 +0500

    update

--- 287.FindtheDuplicateNumber/app.py
+++ 287.FindtheDuplicateNumber/app.py
@@ -2,7 +2,7 @@ class solution:
     def remove(self,nums):
         nums.sort()
         for i in range(len(nums)-1):
-            if nums[i]+nums[i+1]:
+            if nums[i]== nums[i+1]:
                 return nums[i]
         return -1
     
