d34612e776f7bbeeb537408b326d85166e05a985
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-12-04 14:46:56 +0500

    update

--- Tree/226.InvertBinaryTree/app.py
+++ Tree/226.InvertBinaryTree/app.py
@@ -7,7 +7,8 @@ class Solution:
     def invert(self,root):
         if not root:
             return None
-        temp = root.left ,root.right = root.right, root.left
+        temp = root.left  = root.right 
+        root.left =root.right
         self.invert(root.left)
         self.invert(root.right)
         return root
