980dc33387c3879bdb0a4b9b3f6c57d173362eb3
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-12-13 18:09:01 +0500

    update

--- Tree/108.ConvertSortedArraytoBinary/app.py
+++ Tree/108.ConvertSortedArraytoBinary/app.py
@@ -9,4 +9,7 @@ class Solution:
         def helper(l,r):
             if l>r:
                 return None
+            m = (l+r)//2
             root = TreeNode(nums[m])
+            root.left = helper(l,m-1)
+            root.right = helper(m+1,r)
