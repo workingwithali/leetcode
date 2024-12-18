700e3d01ba66438e56bd4503a76e01b4d3fbbf55
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-12-18 23:19:03 +0500

    update

--- Tree/2331.EvaluateBooleanBinaryTree/app.py
+++ Tree/2331.EvaluateBooleanBinaryTree/app.py
@@ -1,2 +1,10 @@
+class NodeTree:
+    def __init__(self,val = 0 , left = None ,right = None):
+        self.val = val
+        self.left = left
+        self.right = right
 class Solution:
-    
+    def boolen(self,root):
+        if not root.left:
+            return root.val == 1
+        
