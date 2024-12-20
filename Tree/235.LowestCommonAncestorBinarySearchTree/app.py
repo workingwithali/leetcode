11a29d7608f9eee81ab9644f1ab78bf1fe21cd5a
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-12-20 16:55:24 +0500

    update

--- Tree/235.LowestCommonAncestorBinarySearchTree/app.py
+++ Tree/235.LowestCommonAncestorBinarySearchTree/app.py
@@ -1,5 +1,5 @@
 class NodeTree:
-    def __init__(self,val = 0):
+    def __init__(self,val = 0,right = None,left = None):
         self.val = val
         self.right = right
         self.left = left
