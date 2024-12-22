e6bcfe705a6a4f6956f934626b1f35f2f7e6c493
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2024-12-22 16:50:07 +0500

    update

--- Tree/450.DeleteNodeBST/app.py
+++ Tree/450.DeleteNodeBST/app.py
@@ -3,3 +3,18 @@ class TreeNode:
         self.val = val
         self.right = right
         self.left = left
+
+class Solution :
+    def delete(self,root,key):
+        if not root:
+            return None
+        if key > root.val:
+            root.right = self.delete(root.right,key)
+        elif key< root.val:
+            root.left = self.delete(root.left,key)
+        else:
+            if not root.left:
+                return root.right
+            elif not root.right:
+                return root.left
+            
