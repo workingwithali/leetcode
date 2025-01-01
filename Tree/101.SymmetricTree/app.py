8f6f478ae8eb78689da3a60a2a8cdf6de3e91779
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-01-01 14:48:27 +0500

    update

--- Tree/101.SymmetricTree/app.py
+++ Tree/101.SymmetricTree/app.py
@@ -1,8 +1,14 @@
 class TreeNode:
-    def __init__(self):
+    def __init__(self,val = 0,right = None,left=None):
         self.val =val
         self.left = left
+        self.right = right
 
 class Solution:
     def symmetric(self,root):
-        
+        def hepler(rootL,rootR):
+            if not rootL and not rootR:
+                return True
+            if not rootL or rootR:
+                return False
+            return (rootL.val == rootR and hepler(rootL.left,rootR.right)and hepler(rootL.right,rootR.left))
