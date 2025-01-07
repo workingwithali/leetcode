87c5b05688254729c1dec6c9eeb97d66d110d774
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-01-07 16:15:50 +0500

    update

--- array/2215.FindDifferenceTwoArrays/app.py
+++ array/2215.FindDifferenceTwoArrays/app.py
@@ -1,29 +1,31 @@
 from typing import List
 
 class Solution:
-    def array(self,nums1,nums2):
-        res = [set(),set()]
+    def array(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+        res = [set(), set()]
         for num1 in nums1:
-            fount = True
+            found = True
             for num2 in nums2:
                 if num1 == num2:
                     found = False
                     break
-            if fount:
+            if found:
                 res[0].add(num1)
+        
         for num2 in nums2:
-            fount = True
+            found = True
             for num1 in nums1:
                 if num1 == num2:
                     found = False
                     break
-            if fount:
+            if found:
                 res[1].add(num2)
-        return [list(res[0]),list(res(1))]
-    
+        
+        return [list(res[0]), list(res[1])]
+
+# Example usage
 solution = Solution()
-nums1 = [1,2,3]
-nums2 = [2,4,6]
-result = solution.array(nums1,nums2)
+nums1 = [1, 2, 3]
+nums2 = [2, 4, 6]
+result = solution.array(nums1, nums2)
 print(result)
-            
