c17b6995e97bb2da7377cf7f364eaa7fd61bf076
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-01-07 16:04:09 +0500

    update

--- array/2215.FindDifferenceTwoArrays/app.py
+++ array/2215.FindDifferenceTwoArrays/app.py
@@ -1,4 +1,10 @@
 class Solution:
     def array(self,nums1,nums2):
         res = [set(),set()]
-        for num1
+        for num1 in nums1:
+            fount = True
+            for num2 in nums2:
+                if num1 == num2:
+                    found = False
+                    break
+            
