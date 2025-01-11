7953ada8f68f12d4b6484aa875e5d5f2885b2e44
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-01-11 22:38:25 +0500

    update

--- heap&PriorityQueue/973.ClosestPointsOrigin/app.py
+++ heap&PriorityQueue/973.ClosestPointsOrigin/app.py
@@ -1,10 +1,16 @@
 import heapq
 class solution:
-    def closepoint(self ,points):
+    def closepoint(self ,points,k):
         minheap = []
         for x,y in points:
             dist = (x**2)+(y**2)
             minheap.append([dist,x,y])
         heapq.heapify(minheap)
-        res     
+        res  = []
+        while k > 0:
+            dist , x ,y = heapq.heappop(minheap)
+            res.append([x,y])
+            k -=1
+        return res
+
 
