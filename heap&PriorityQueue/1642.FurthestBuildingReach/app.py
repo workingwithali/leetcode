db089584684ec270febb0813e06cf9956792c2b6
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-01-14 18:47:49 +0500

    update

--- heap&PriorityQueue/1642.FurthestBuildingReach/app.py
+++ heap&PriorityQueue/1642.FurthestBuildingReach/app.py
@@ -1,7 +1,11 @@
+import heapq
 class solution:
     def build(self,heights,bricks,ladders):
         heap = [] #maxheap
         for i in range(len(heights)-1):
             diff = heights[i+1]-heights[i]
             if diff <= 0:
-                
+                continue
+            bricks -= diff
+            heapq.heappush(heap,-diff)
+            if bricks 
