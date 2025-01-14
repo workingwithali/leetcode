90ecd0d9592420fadb3c01d7370e6fb2b0adc9d3
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-01-14 11:14:17 +0500

    update

--- heap&PriorityQueue/1481.LeastNumberUniqueIntegersRemovals/app1.py
+++ heap&PriorityQueue/1481.LeastNumberUniqueIntegersRemovals/app1.py
@@ -1,10 +1,14 @@
 from collections import Counter
 import heapq
 class Solution:
-    def freq(self,arr):
+    def freq(self,arr,k):
         freq = Counter(arr)
         heap = list(freq)
         heapq.heapify(heap)
         res = len(heap)
+        while k > 0 and heap:
+            f = heapq.heappop(heap)
+            
+
 
         return res
