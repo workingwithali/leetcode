8639ca47715637d32a012d895e5e0a3b81721300
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-01-14 18:40:41 +0500

    update

--- heap&PriorityQueue/1642.FurthestBuildingReach/app.py
+++ heap&PriorityQueue/1642.FurthestBuildingReach/app.py
@@ -1,2 +1,6 @@
 class solution:
     def build(self,heights,bricks,ladders):
+        heap = [] #maxheap
+        for i in range(len(heights)-1):
+            diff = heights[i+1]-heights[i]
+            
