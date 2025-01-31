5f9fd0ab3085034186201e146d3ced4c0a92306a
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-01-31 12:49:10 +0500

    update

--- 704.BinarySearch/app.cpp
+++ 704.BinarySearch/app.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <vector>
-using namespace;
+using namespace std;
 class Solution{
 	public:
-		int search(vector<int>arr,int tar,int s, int e){
+		int search(vector<int>&arr,int tar,int s, int e){
 			if (s <= e){
 				int mid = (s+e)/2;
 				if (arr[mid] == tar) return mid;
@@ -12,14 +12,18 @@ class Solution{
 				}else{
 					return search(arr,tar,s,mid-1);
 				}
-				return -1;
 			}
+			return -1;
 		}
 };
 
 
 
 int main(){
+	vector<int> nums = {0,3,5,9,12};
+	int target = 9;
+	Solution s;
+	cout <<s.search(nums,target,0,nums.size()-1);
 	
 	return 0;
 }
