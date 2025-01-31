71bb57621c9f1b15a83d8d53c8f1c1b993f3793e
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-01-31 12:28:13 +0500

    update

---
+++ 704.BinarySearch/app.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace;
+class Solution{
+	public:
+		int search(vector<int>arr,int tar,int s, int e){
+			if (s <= e){
+				int mid = (s+e)/2;
+			}
+		}
+};
+
+
+
+int main(){
+	return 0;
+}
