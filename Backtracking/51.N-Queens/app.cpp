fea68e7bd440e704508d009d891cb63ce6106ac9
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-02-04 15:44:41 +0500

    update

--- Backtracking/51.N-Queens/app.cpp
+++ Backtracking/51.N-Queens/app.cpp
@@ -3,7 +3,17 @@
 using namespace std;
 class Solution{
     public:
-        void queen(vector<int> &board,int roq,int n, vector<vector<int>> &ans)
+        void queen(vector<int> &board,int row,int n, vector<vector<int>> &ans){
+            if (row== n){
+                ans.push_back(board);
+                return;
+            }
+            for (int j=0;j<n;j++){
+                if(issafe(board,row,j,ans)){
+                    board[row][j] = 'Q'
+                }
+            }
+        }
             vector<vector<int>> solveNQueens(int n)
         {
             vector<int> board(n,string(n,'.'));
