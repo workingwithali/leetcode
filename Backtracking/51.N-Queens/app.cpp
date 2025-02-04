63c7f26aa2b5620673b8627f24d54f4cfee99e9e
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-02-04 15:48:26 +0500

    update

--- Backtracking/51.N-Queens/app.cpp
+++ Backtracking/51.N-Queens/app.cpp
@@ -3,8 +3,31 @@
 using namespace std;
 class Solution{
     public:
-        bool issafe(vector<int> &board,int row,int col, vector<vector<int>> &ans){
-            
+        bool issafe(vector<string> &board, int row, int col, int n)
+        {
+
+            for (int j = 0; j < n; j++)
+            {
+                if (board[j][col] == 'Q')
+                {
+                    return false;
+                }
+            }
+            for (int i = row, j = col; i >= 0 && j >= 0; i--, j--)
+            {
+                if (board[i][j] == 'Q')
+                {
+                    return false;
+                }
+            }
+            for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++)
+            {
+                if (board[i][j] == 'Q')
+                {
+                    return false;
+                }
+            }
+            return true;
         }
         void queen(vector<int> &board,int row,int n, vector<vector<int>> &ans){
             if (row== n){
