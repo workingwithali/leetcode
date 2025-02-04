cfbc3bbbf853107fd570316d1d525510dfd6ed90
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-02-04 15:41:54 +0500

    update

--- Backtracking/51.N-Queens/app.cpp
+++ Backtracking/51.N-Queens/app.cpp
@@ -3,9 +3,12 @@
 using namespace std;
 class Solution{
     public:
-        vector<vector<int>> solveNQueens(int n){
+        void queen(vector<int> &board,int roq,int n, vector<vector<int>> &ans)
+            vector<vector<int>> solveNQueens(int n)
+        {
             vector<int> board(n,string(n,'.'));
-            vector<vector<int>>ans
+            vector<vector<int>>ans;
+            return ans;
         }
 }
 int main(){
