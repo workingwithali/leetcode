a7838a2a6e125ae61541ab94450a1e3904368914
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-02-06 11:39:50 +0500

    update

--- Backtracking/39.CombinationSum/app.cpp
+++ Backtracking/39.CombinationSum/app.cpp
@@ -13,9 +13,15 @@ class Solution{
             if (s.find(combin)==s.end()){
                 ans.push_back(combin);
                 s.insert(combin);
+                }
+                return;
             }
+            combin.push_back(arr[i]);
+            getans(arr, target-arr[i], i+1, ans, combin);
+            getans(arr, target-arr[i], i, ans, combin);
+            combin.pop_back();
+            getans(arr, target, i+1, ans, combin);
         }
-    }
         vector<vector<int>> sum(vector &arr, int target)
         {
             vector<vector<int>> ans;
