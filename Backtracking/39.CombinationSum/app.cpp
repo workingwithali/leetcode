e47d110791fec942024f2d9ac594cc8cf411840b
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-02-06 11:35:36 +0500

    update

---
+++ Backtracking/39.CombinationSum/app.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+#include<set>
+using namespace std;
+class Solution{
+    public:
+    set<vector<int>> s;
+    void getans(vector &arr, int target, int i, vector<vector<int>> &ans, vector<int> &combin){
+        if(i==ar.size()||){
+            return;
+        }
+    }
+        vector<vector<int>> sum(vector &arr, int target)
+    {
+        vector<vector<int>> ans;
+        vector<int> combin;
+        getans(arr,target,0,ans,combin);
+        return ans;
+    }
+}
