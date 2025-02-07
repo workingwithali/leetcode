90cf0c2a265b2840ce980162846665cc8cfb7fe7
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-02-07 15:08:02 +0500

    update

--- Backtracking/131.PalindromePartitioning/app.cpp
+++ Backtracking/131.PalindromePartitioning/app.cpp
@@ -3,10 +3,15 @@
 using namespace std;
 class Solution{
     public:
+    bool ispalin(stirng s){
+        string s2 = s;
+        reverse(s2.begin(),s2.end());
+    }
     vector<vector<string>> palindorme(string s){
         vector<string> partitions;
         vector<vector<string> ans;
-        getpalin(s,)
+        getpalin(s,partitions,ans);
+        return ans;
     }
 }
 int main(){
