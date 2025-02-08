357e69458efee23858e0facce88beb40bdb5239f
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-02-08 13:37:47 +0500

    update

--- Backtracking/2596.CheckKnightTourConfiguration/app.cpp
+++ Backtracking/2596.CheckKnightTourConfiguration/app.cpp
@@ -3,9 +3,13 @@
 using namespace std;
 class Solution{
     public:
+        isvaild(vector<vector<int>>grid,int r, int c ,int n,int expval){
+            if (r < 0|| c < 0|| r >=n || c>= ||)
+        }
         checkgrid(vector<vector<int>> grid){
             return isvaild(grid, 0 ,0 ,gird.size(),0);
         }
+
 }
 
 
