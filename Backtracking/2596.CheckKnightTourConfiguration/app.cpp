5a3213d871d460ab1782efeb3fdceb956295c2ed
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-02-08 13:27:43 +0500

    update

---
+++ Backtracking/2596.CheckKnightTourConfiguration/app.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution{
+    public:
+        checkgrid(vector<vector<int>> grid){
+            return isvaild(grid, 0 ,0 ,gird.size(),0);
+        }
+}
+
+
+int main(){
+
+
+    return 0;
+}
