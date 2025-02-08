baa1dfc14090fa9a89229a91de200a2436970400
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-02-08 13:41:17 +0500

    update

--- Backtracking/2596.CheckKnightTourConfiguration/app.cpp
+++ Backtracking/2596.CheckKnightTourConfiguration/app.cpp
@@ -4,7 +4,20 @@ using namespace std;
 class Solution{
     public:
         isvaild(vector<vector<int>>grid,int r, int c ,int n,int expval){
-            if (r < 0|| c < 0|| r >=n || c>= ||)
+            if (r < 0|| c < 0|| r >=n || c>= || grid[r][c]!= expval){
+                return false;
+            }
+            if (expval == n*n-1){
+                return true;
+            }
+            bool ans1 = isvaild(grid, r-2,c+1,n,expval+1);
+            bool ans2 = isvaild(grid, r-2,c+1,n,expval+1);
+            bool ans3 = isvaild(grid, r-2,c+1,n,expval+1);
+            bool ans4 = isvaild(grid, r-2,c+1,n,expval+1);
+            bool ans5 = isvaild(grid, r-2,c+1,n,expval+1);
+            bool ans6 = isvaild(grid, r-2,c+1,n,expval+1);
+            bool ans7 = isvaild(grid, r-2,c+1,n,expval+1);
+            bool ans8 = isvaild(grid, r-2,c+1,n,expval+1);
         }
         checkgrid(vector<vector<int>> grid){
             return isvaild(grid, 0 ,0 ,gird.size(),0);
