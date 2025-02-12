f96a4927c945da46006b5fa7ab51b3270fa61023
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-02-12 16:43:36 +0500

    update

--- Backtracking/79.WordSearch/app.cpp
+++ Backtracking/79.WordSearch/app.cpp
@@ -1,22 +1,23 @@
-#include<iosrtream>
+#include<iostream>
 #include<vector>
 using namespace std;
 class Solution{
     public:
-    int row col;
-    boot helper(vector<vector<string>> board, string word, int r ,int c , int i){
-        if (i== word.size()){
-            return true;
-        }
-        if (r<0||c<0||r>=row||c>=col||board[r][c]!=word[i]||board[r][c]=='#'){
-            return false;
-        }
-        board[r][c] = '#';
-        bool res = helper(board, word, r - 1, c, i + 1) || helper(board, word, r + 1, c, i + 1) || helper(board, word, r , c-1, i + 1) || helper(board, word, r , c+1, i + 1) ;
-        board[r][c] = word[i];
-        return res;
-    }
-    bool exit(vector<vector<string>>board, stirng word){
+    	int row;
+		int col;
+    	bool helper(vector<vector<string>> board, string word, int r ,int c , int i){
+        	if (i== word.size()){
+            	return true;
+        	}
+        	if (r<0||c<0||r>=row||c>=col||board[r][c] != word[i]||board[r][c]=='#'){
+           		return false;
+        	}
+        	board[r][c] = '#';
+        	bool res = helper(board, word, r - 1, c, i + 1) || helper(board, word, r + 1, c, i + 1) || helper(board, word, r , c-1, i + 1) || helper(board, word, r , c+1, i + 1) ;
+        	board[r][c] = word[i];
+        	return res;
+    	}
+    	bool exit(vector<vector<string>>board, string word){
         row = board.size();
         col = board[0].size();
         for(int r = 0 ;r<row; r++ ){
@@ -28,7 +29,7 @@ class Solution{
         }
         return false;
     }
-}
+};
 int main(){
     Solution s;
     board = [ [ "A", "B", "C", "E" ], [ "S", "F", "C", "S" ], [ "A", "D", "E", "E" ] ];
