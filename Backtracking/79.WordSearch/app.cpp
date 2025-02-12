f20b20d6e72c01fc00a501e3993afedfb43a7afc
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-02-12 16:21:04 +0500

    update

---
+++ Backtracking/79.WordSearch/app.cpp
@@ -0,0 +1,12 @@
+#include<iosrtream>
+#include<vector>
+using namespace std;
+class Solution{
+    public:
+    int row col;
+    boot helper(vector<vector<int>> board, string word, int r ,int c , int i){
+        if (i== word.size()){
+            return true
+        }
+    }
+}
