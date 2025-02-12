bf1e895757ae23242fe625de1b8a57eee625a7cb
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-02-12 16:23:02 +0500

    update

--- Backtracking/79.WordSearch/app.cpp
+++ Backtracking/79.WordSearch/app.cpp
@@ -6,7 +6,10 @@ class Solution{
     int row col;
     boot helper(vector<vector<int>> board, string word, int r ,int c , int i){
         if (i== word.size()){
-            return true
+            return true;
+        }
+        if (r<0||c<0||r>=row||c>=col||board[r][c]!=word[i]||board[r][c]=='#'){
+            return false;
         }
     }
 }
