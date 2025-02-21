e439365fd38f8f47bd7dc8bb7fa266326d899cd4
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-02-21 16:07:42 +0500

    update

--- array/169.MajorityElement/app1.cpp
+++ array/169.MajorityElement/app1.cpp
@@ -3,8 +3,23 @@
 using namespace std;
 class Solution{
 	public:
-		int element()
+		int element(vector<int>arr){
+			int n=arr.size();
+			for(int val:arr){
+				int feq =0;
+				for(int ele:arr){
+					if(ele==val){
+						feq++;
+					}
+				}
+				if(feq>n/2)return val;
+			}
+		}
 };
 int main(){
+	vector<int>arr ={2,2,1,1,1,2,2};
+	Solution sol;
+	int ans=sol.element(arr);
+	cout<<ans;
 	
 }
