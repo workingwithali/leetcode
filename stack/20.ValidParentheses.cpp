2d3a44fc4eb2c32424933698b58512fe39ee79f6
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-03-08 18:00:41 +0500

    update

--- stack/20.ValidParentheses.cpp
+++ stack/20.ValidParentheses.cpp
@@ -6,8 +6,22 @@ class Solution{
 		bool vaild(stirng str){
 			stack<char> s;
 			for(int i = 0; i<str.size();i++){
-				if(str[i]=='()'||)
+				if(str[i]=='('||str[i]=='['||str[i]=='{'){
+					s.push(str[i]);
+				}else{
+					if(s.size()==0){
+						return false;
+					}
+					if(s.top()=='('&&str[i]==')'||
+                	s.top()=='['&&str[i]==']'||
+                	s.top()=='{'&&str[i]=='}'){
+                		s.pop();
+					}else{
+						return false;
+					}
+				}
 			}
+			return s.size()==0;
 		}
 };
 
