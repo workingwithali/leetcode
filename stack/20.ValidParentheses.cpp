c5d75071abafa02d301009023b9563d331edd3c5
Ali Rashid <140310758+workingwithali@users.noreply.github.com>
2025-03-08 17:56:14 +0500

    update

--- stack/20.ValidParentheses.cpp
+++ stack/20.ValidParentheses.cpp
@@ -1,1 +1,18 @@
-#
+#include<iostream>
+#include<stack>
+using namespace std;
+class Solution{
+	public:
+		bool vaild(stirng str){
+			stack<char> s;
+			for(int i = 0; i<str.size();i++){
+				if(str[i]=='()'||)
+			}
+		}
+};
+
+
+int main(){
+	
+	return 0;
+}
